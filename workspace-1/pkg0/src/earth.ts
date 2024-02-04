f09e1a6dde68ca166cf63b6227f751a91683cfb6
Rakesh <rake7h@gmail.com>
2024-02-04 10:49:55 +0530

    packsize added

--- workspace-1/pkg0/src/earth.ts
+++ workspace-1/pkg0/src/earth.ts
@@ -3,3 +3,6 @@ import { helper } from 'lib0/helper';
 
 console.log(isNonEmptyArray(['12']))
 console.log(helper())
+console.log(isNonEmptyArray(['12']))
+console.log(helper())
+console.log(helper())
